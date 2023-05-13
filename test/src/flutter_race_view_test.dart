eeed400e35008264a08e920d8eb7952f67896a25
rohan20 <rhntnj@gmail.com>
2023-05-14 01:07:16 +0530

    style: re-arrange imports

--- test/src/flutter_race_view_test.dart
+++ test/src/flutter_race_view_test.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_race_view/flutter_race_view.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('FlutterRaceView', () {
