a85431cbedf6e783938f0037604a40dfb4ffc1f4
rohan20 <rhntnj@gmail.com>
2023-05-22 21:58:12 +0530

    add example to RaceViewDemoPage

--- example/lib/race_view_demo/view/race_view_demo_page.dart
+++ example/lib/race_view_demo/view/race_view_demo_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_race_view/flutter_race_view.dart';
 
 class RaceViewDemoPage extends StatelessWidget {
   const RaceViewDemoPage({super.key});
@@ -15,8 +16,32 @@ class RaceViewDemoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Hello World')),
-      body: const SizedBox(),
+      appBar: AppBar(title: const Text('Race View Demo')),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 16),
+        child: RaceView(
+          data: const [
+            [10, 20, 30, 40, 50],
+            [125, 135, 80, 70, 150],
+            [205, 173, 120, 370, 260],
+            [325, 483, 620, 570, 460],
+          ],
+          dataColumnNames: const ['A', 'B', 'C', 'D', 'E'],
+          dataColumnColors: [
+            Colors.red.shade200,
+            Colors.green.shade200,
+            Colors.blue.shade200,
+            Colors.yellow.shade200,
+            Colors.purple.shade200,
+          ],
+          dataRowNames: const [
+            'Year 2017',
+            'Year 2018',
+            'Year 2019',
+            'Year 2020',
+          ],
+        ),
+      ),
     );
   }
 }
